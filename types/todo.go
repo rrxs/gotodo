e143c95b842a215a1250e424114c23651143e9d7
Robson Xavier <rbsmail93@gmail.com>
2024-01-09 22:17:29 -0300

    insert type todo response

--- types/todo.go
+++ types/todo.go
@@ -1,10 +1,20 @@
 package types
 
 import (
+	"time"
+
 	"gorm.io/gorm"
 )
 
 type Todo struct {
 	gorm.Model
-	Description string
+	Title string
+}
+
+type TodoResponse struct {
+	ID        uint      `json:"id"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+	DeletedAt time.Time `json:"deteledAt,omitempty"`
+	Title     string    `json:"description"`
 }
